8df02e9e1fd2a9ac0d397a541f4c3f16fa705dce
Michał "phoe" Herda <phoe@disroot.org>
2018-10-27 12:15:41 +0200

    meet Dziekan in the middle of the night

--- rooms/korytarz.cpp
+++ rooms/korytarz.cpp
@@ -4,8 +4,18 @@
 
 Room korytarz(GameData& gameData) {
 
-std::cout << "Idziesz przez korytarz. Serwerownia jest otwarta. "
-          << "Wchodzisz do niej." << std::endl;
+std::cout << "Idziesz przez korytarz. Spotykasz Dziekana." << std::endl
+          << "Uciekasz przed nim?" << std::endl;
+
+    if(yes()) {
+        std::cout << "Przed Dziekanem się nie ucieka." << std::endl;
+        gameData.alive = false;
+    } else {
+        std::cout << "Mówisz, \"Dzień dobry, panie Dziekanie\" "
+                  << "i przechodzisz koło niego." << std::endl
+                  << "Serwerownia jest otwarta. "
+                  << "Wchodzisz do niej." << std::endl;
+    }
 
     return SERWEROWNIA;
 }
