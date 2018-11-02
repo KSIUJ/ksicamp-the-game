ab7c11c8c89f9d6d5571e66b8b7c0b3e36fc099d
Marek "seqre" Grzelak <seqre@disroot.org>
2018-11-02 23:24:11 +0100

    Handled malfunctioning input validation

--- rooms/windyTylne.cpp
+++ rooms/windyTylne.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "../main.hpp"
 
@@ -9,21 +10,21 @@ void start(GameData& gameData){
 }
 
 Room wybor_dzialania(GameData& gameData, short i_c){
-    char wybor;
+    std::string wybor;
     while(true){
-        std::cout << "Co robisz?" << std::endl
-              << "1. Korzystam z windy i zjeżdżam na parter" << std::endl //TODO: Add lift repair minigame
-              << "2. Idę do windy z przodu budynku" << std::endl
-              << "3. Idę do biblioteki" << std::endl
-              << "4. Idę do garażu (i robię przyps)" << std::endl
-              << "5. Wracam na korytarz" << std::endl;
+        std::cout << "Co robisz?" << std::endl;
+        wait_ms(200);
+        std::cout << "1. Korzystam z windy i zjeżdżam na parter" << std::endl //TODO: Add lift repair minigame
+                  << "2. Idę do windy z przodu budynku" << std::endl
+                  << "3. Idę do biblioteki" << std::endl
+                  << "4. Idę do garażu (i robię przyps)" << std::endl
+                  << "5. Wracam na korytarz" << std::endl;
     
         std::cout << "> ";
         std::cin >> wybor;
         std::cout << std::endl;
 
-        switch(wybor){
-        case '1':
+        if(wybor == "1"){
             if(!gameData.winda_repaired) {
                 if(i_c == 8){
                     std::cout << "Przypominasz sobie o chińskich korzeniach i stwierdzasz, że za 8 sprawdzeniem winda na pewno będzie działać"
@@ -31,6 +32,7 @@ Room wybor_dzialania(GameData& gameData, short i_c){
                     wait_s(1);
                     std::cout << "Naciskasz przycisk przywołania windy i w tym momencie śmiertelnie razi Cię prąd"
                               << std::endl;
+                    wait_s(1);
                     gameData.alive = false;
                     return ZEWNATRZ;
                 }
@@ -38,29 +40,22 @@ Room wybor_dzialania(GameData& gameData, short i_c){
                 std::cout << "Ślepy, czy głupi? Popsuta jest." << std::endl;
                 wait_s(1);
                 i_c++;
-                
-                break;
             } else return WINDA_PARTER;
         
-        case '2':
-            return WINDA_PIETRO;
+        } else if(wybor == "2") return WINDA_PIETRO;
 
-        case '3':
-            return BIBLIOTEKA;
+        else if(wybor == "3") return BIBLIOTEKA;
 
-        case '4':
-            return GARAZ;
+        else if(wybor == "4") return GARAZ;
 
-        case '5':
-            return KORYTARZ;
-
-        default:
+        else if(wybor == "5") return KORYTARZ;
+        
+        else { 
             std::cout << std::endl
                       << "To nie jest miejsce, którego szukasz" << std::endl
                       << "*Obi-Wan Kenobi's hand wave*" << std::endl
                       << "Wybierz raz jeszcze..." << std::endl
                       << "...tym razem mądrze, jeśli można." << std::endl << std::endl;
-            break;
         }
     }
 }
