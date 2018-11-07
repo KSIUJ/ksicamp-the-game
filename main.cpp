6a4e76312e79ac2674d1fe9e82ca9fc02f85f7a3
Mateusz Gwóźdź <matgw123@gmail.com>
2018-11-07 21:54:01 +0100

    indent with with tabs.

--- main.cpp
+++ main.cpp
@@ -3,34 +3,34 @@
 #include "main.hpp"
 
 void start() {
-    std::cout << std::endl 
-              << "        **** KSICAMP - THE GAME ****" << std::endl
-              << "        designed by the KSICAMP team" << std::endl 
-              << std::endl
-              << "Obudziłeś się." << std::endl;
+	std::cout << std::endl 
+			  << "        **** KSICAMP - THE GAME ****" << std::endl
+			  << "        designed by the KSICAMP team" << std::endl 
+			  << std::endl
+			  << "Obudziłeś się." << std::endl;
 }
 
 void finish(GameData& gameData) {
-    std::cout << "Koniec gry." << std::endl
-              << "Wynik: " << (gameData.alive ? "Wygrałeś." : "Przegrałeś.") << std::endl
-              << "W kieszeniach masz:" << std::endl;
-    if (gameData.inventory.empty()) {
-        std::cout << "* nic" << std::endl;
-    } else {
-        for (std::string item : gameData.inventory) {
-            std::cout << "* " << item << std::endl;
-        }
-    }
+	std::cout << "Koniec gry." << std::endl
+			  << "Wynik: " << (gameData.alive ? "Wygrałeś." : "Przegrałeś.") << std::endl
+			  << "W kieszeniach masz:" << std::endl;
+	if (gameData.inventory.empty()) {
+		std::cout << "* nic" << std::endl;
+	} else {
+		for (std::string item : gameData.inventory) {
+			std::cout << "* " << item << std::endl;
+		}
+	}
 }
 
 int main() {
-    start();
+	start();
 
-    GameData gameData(KSI);
-    do {
-        gameData.room = (gameData.functions[gameData.room])(gameData);
-        std::cout << std::endl;
-    } while (gameData.room != ZEWNATRZ && gameData.alive);
+	GameData gameData(KSI);
+	do {
+		gameData.room = (gameData.functions[gameData.room])(gameData);
+		std::cout << std::endl;
+	} while (gameData.room != ZEWNATRZ && gameData.alive);
 
-    finish(gameData);
+	finish(gameData);
 }
