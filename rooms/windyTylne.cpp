630e8d82fce083db310ba0c57e769b21ca59e2ce
Adam Pardyl <adam@pardyl.com>
2018-11-07 22:39:05 +0100

    Cleanup code

--- rooms/windyTylne.cpp
+++ rooms/windyTylne.cpp
@@ -2,29 +2,21 @@
 
 #include "../main.hpp"
 
-Room windyTylne(__attribute__((unused)) GameData &gameData)
-{
-    std::cout << "Jesteś w windach tylnych" << std::endl
-              << "Czy chcesz iść do biblioteki?" << std::endl;
-    if (yes())
-    {
-        return BIBLIOTEKA;
-    }
-    else if (yes())
-    {
-        std::cout << "Czy chcesz iść do garażu?" << std::endl;
-        return GARAZ;
-    }
-    else if (yes())
-    {
-        std::cout << "Czy chcesz iść do windy na piętrze?" << std::endl;
-        return WINDA_PIETRO;
-    }
-    else if (yes())
-    {
-        std::cout << "Czy chcesz iść do windy na parterze?" << std::endl;
-        return WINDA_PARTER;
-    }
-    else
-        return WINDY_TYLNE;
+Room windyTylne(__attribute__((unused)) GameData &gameData) {
+  std::cout << "Jesteś w windach tylnych" << std::endl
+            << "Czy chcesz iść do biblioteki?" << std::endl;
+  if (yes()) {
+    return BIBLIOTEKA;
+  } else if (yes()) {
+    std::cout << "Czy chcesz iść do garażu?" << std::endl;
+    return GARAZ;
+  } else if (yes()) {
+    std::cout << "Czy chcesz iść do windy na piętrze?" << std::endl;
+    return WINDA_PIETRO;
+  } else if (yes()) {
+    std::cout << "Czy chcesz iść do windy na parterze?" << std::endl;
+    return WINDA_PARTER;
+  } else {
+    return WINDY_TYLNE;
+  }
 }
