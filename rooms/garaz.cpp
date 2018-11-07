9f5ce55c8dbbf5884540792cd69f0fe4b31344a9
Michał "phoe" Herda <phoe@disroot.org>
2018-11-07 22:10:47 +0100

    Revert cancerous "indent with with tabs." Fix bug.
    This reverts commit 9171be2dacf92def95c7d46a1298de56b5409029.

--- rooms/garaz.cpp
+++ rooms/garaz.cpp
@@ -4,27 +4,27 @@
 
 Room garaz (GameData& gameData)
 {
-	std::cout<<"Jestes w garazu. Jeden z samochodow ma zapalone swiatla."<<std::endl;
-	std::cout<<"Moze ktos jest w srodku?"<<std::endl;
-	std::cout<<"Podchodzisz sprawdzic?"<<std::endl;
+    std::cout<<"Jestes w garazu. Jeden z samochodow ma zapalone swiatla."<<std::endl;
+    std::cout<<"Moze ktos jest w srodku?"<<std::endl;
+    std::cout<<"Podchodzisz sprawdzic?"<<std::endl;
 
-	if(yes())
-	{
-		std::cout<<"W samochodzie siedzi Dziekan. Proponuje podwozke do Kauflanda"<<std::endl;
-		return ZEWNATRZ;
-	}
+    if(yes())
+    {
+        std::cout<<"W samochodzie siedzi Dziekan. Proponuje podwozke do Kauflanda"<<std::endl;
+        return ZEWNATRZ;
+    }
 
-	std::cout<<"Samochod odjezdza, zostajesz w calkowitej ciemnosci."<<std::endl;
+    std::cout<<"Samochod odjezdza, zostajesz w calkowitej ciemnosci."<<std::endl;
 
-	if(gameData.zaionc)
-	{
-		std::cout<<"Z rogu garazu slychac szybki oddech..."<<std::endl;
-		wait_ms(300);
-		std::cout<<"TO ZAIONC"<<std::endl;
-		std::cout<<"Nie masz dokad uciec"<<std::endl;
-	}
+    if(gameData.zaionc)
+    {
+        std::cout<<"Z rogu garazu slychac szybki oddech..."<<std::endl;
+        wait_ms(300);
+        std::cout<<"TO ZAIONC"<<std::endl;
+        std::cout<<"Nie masz dokad uciec"<<std::endl;
+    }
 
-	std::cout<<"Drzwi garazu sa zamkniete, mozesz tylko wrocic do windy."<<std::endl;
+    std::cout<<"Drzwi garazu sa zamkniete, mozesz tylko wrocic do windy."<<std::endl;
 
-	return WINDY_TYLNE;
+    return WINDY_TYLNE;
 }
