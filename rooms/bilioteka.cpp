125e46cd5d7b1732ca7002cc1575ca0f1ed825f2
Jakub Gadawski <jakub.gadaw@outlook.com>
2018-11-07 23:55:46 +0100

    some more changes

--- rooms/bilioteka.cpp
+++ rooms/bilioteka.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "../main.hpp"
 
-Room biblioteka(GameData &gameData)
-{
-  if (gameData.zaionc)
-  {
+Room biblioteka(GameData &gameData) {
+  if (gameData.zaionc) {
     std::cout << "Wpadasz do bibliteki, ZAIONC wciąż Cię goni." << std::endl
               << "Ukrywasz się między regałami, licząc, że ZAIONC nie wykryje "
                  "Cię swoim ekstra nosem."
@@ -18,30 +16,28 @@ Room biblioteka(GameData &gameData)
     return FAIL;
   }
 
-  std::cout << "Trafiasz do biblioteki. Słyszałeś o klątwie Cormena która dotknie "
-               "każdego, kto pobierze PDF-a jego książki."
-            << std::endl
-            << "Kody tego nieszczęśnika nigdy nie wejdą na CodeForces, stanie się "
-               "stworzeniem tak nieczystym, że biały pył kredy nie będzie na "
-               "nim osiadał."
-            << std::endl
-            << "W bibliotece nie ma jednak Wprowadzenia do Algorytmów. Ponoszą Cię nerwy, "
-               "czy szukasz PDF-a w internecie?"
-            << std::endl;
-  if (yes())
-  {
-    std::cout
-        << "Źle postąpiłeś. Cormen dręczy Cię teraz w snach."
-        << std::endl
-        << "Załamany postanawiasz skończyć ze sobą. Rzucasz się z okna."
-        << std::endl;
+  std::cout
+      << "Trafiasz do biblioteki. Słyszałeś o klątwie Cormena która dotknie "
+         "każdego, kto pobierze PDF-a jego książki."
+      << std::endl
+      << "Kody tego nieszczęśnika nigdy nie wejdą na CodeForces, stanie się "
+         "stworzeniem tak nieczystym, że biały pył kredy nie będzie na "
+         "nim osiadał."
+      << std::endl
+      << "W bibliotece nie ma jednak Wprowadzenia do Algorytmów. Ponoszą Cię "
+         "nerwy, "
+         "czy szukasz PDF-a w internecie?"
+      << std::endl;
+  if (yes()) {
+    std::cout << "Źle postąpiłeś. Cormen dręczy Cię teraz w snach." << std::endl
+              << "Załamany postanawiasz skończyć ze sobą. Rzucasz się z okna."
+              << std::endl;
     wait_s(2);
     gameData.alive = false;
     return FAIL;
-  }
-  else
-  {
-    std::cout << "Wymyślasz algorytm z lepszą złożonością niż w Cormenie. Tłum szaleje ma twój widok."
+  } else {
+    std::cout << "Wymyślasz algorytm z lepszą złożonością niż w Cormenie. Tłum "
+                 "szaleje ma twój widok."
               << std::endl;
     wait_s(2);
     return WINDY_TYLNE;
