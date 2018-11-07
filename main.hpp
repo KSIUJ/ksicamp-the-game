dd80b129f6e961548e1cd22b77ed3fc08d2ba4c4
Kacper Stysiński <kaacper1112@gmail.com>
2018-11-07 21:05:02 +0100

    Added garaz

--- main.hpp
+++ main.hpp
@@ -51,7 +51,7 @@ public:
         this->functions[WINDA_PIETRO] = &windaPietro;
         this->functions[WINDA_PARTER] = &windaParter;
         this->functions[WINDY_TYLNE] = &fail;
-        this->functions[GARAZ] = &fail;
+        this->functions[GARAZ] = &garaz;
         this->functions[ZEWNATRZ] = &fail;
         this->functions[FAIL] = &fail;
     }
