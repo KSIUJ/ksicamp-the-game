6d1a4695ebba3b108aa2a53957e9d5bad3f0eb71
Adam Pardyl <adam@pardyl.com>
2018-11-07 22:39:05 +0100

    Cleanup code

--- events.cpp
+++ events.cpp
@@ -5,22 +5,21 @@
 
 #include "main.hpp"
 
-Room event_dziekan (GameData& gameData, Room dest) {
-// jesli przetrwa spotkanie z dziekanem
-	std::cout << "Spotykasz Dziekana." << std::endl	
+Room event_dziekan(GameData& gameData, Room dest) {
+  // jesli przetrwa spotkanie z dziekanem
+  std::cout << "Spotykasz Dziekana." << std::endl
             << "Uciekasz przed nim?" << std::endl;
 
-    if(yes()) {
-        std::cout << "Przed Dziekanem się nie ucieka." << std::endl;
-        gameData.alive = false;
-    } else {
-        std::cout << "Mówisz, \"Dzień dobry, panie Dziekanie\" "
-                  << "i przechodzisz koło niego." << std::endl;
-        return dest;    
-    }
+  if (yes()) {
+    std::cout << "Przed Dziekanem się nie ucieka." << std::endl;
+    gameData.alive = false;
+  } else {
+    std::cout << "Mówisz, \"Dzień dobry, panie Dziekanie\" "
+              << "i przechodzisz koło niego." << std::endl;
+    return dest;
+  }
 
-	return FAIL;
-
+  return FAIL;
 }
 
-#endif // EVENTS
+#endif  // EVENTS
