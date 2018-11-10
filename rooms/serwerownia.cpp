515542a5288a2fff51551012f1dfa90e87312fb0
Jakub Gadawski <jakub.gadaw@outlook.com>
2018-11-10 11:06:36 +0100

    garage and library fixed

--- rooms/serwerownia.cpp
+++ rooms/serwerownia.cpp
@@ -3,19 +3,19 @@
 #include "../main.hpp"
 
 Room serwerownia(GameData& gameData) {
-    std::cout << "Wchodziś do serwerowni, nikogo tu nie ma" << std::endl;
+  std::cout << "Wchodziś do serwerowni, nikogo tu nie ma" << std::endl;
 
-    if (gameData.inventory.find("młotek") == gameData.inventory.end()) {
-        std::cout  << "Na podłodze leży młotek." << std::endl
-                   << "Bierzesz ją?" << std::endl;
-        if (yes()) {
-            std::cout << "Wziąłeś młotek i wsadziłeś do kieszeni." << std::endl;
-            gameData.inventory.insert("młotek");
-        }
-    } else {
-        std::cout << "Nic innego tu nie ma." << std::endl;
+  if (gameData.inventory.find("młotek") == gameData.inventory.end()) {
+    std::cout << "Na podłodze leży młotek." << std::endl
+              << "Bierzesz ją?" << std::endl;
+    if (yes()) {
+      std::cout << "Wziąłeś młotek i wsadziłeś do kieszeni." << std::endl;
+      gameData.inventory.insert("młotek");
     }
-    std::cout << "Wracasz do korytarza." << std::endl;
+  } else {
+    std::cout << "Nic innego tu nie ma." << std::endl;
+  }
+  std::cout << "Wracasz do korytarza." << std::endl;
 
-    return KORYTARZ;
+  return KORYTARZ;
 }
