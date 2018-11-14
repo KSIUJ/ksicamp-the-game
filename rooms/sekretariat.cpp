afebc7283b2908507ff6434f9dfcd302a24c9524
skypio <p.skibinski2@gmail.com>
2018-11-14 14:00:36 +0100

    kulturalna version

--- rooms/sekretariat.cpp
+++ rooms/sekretariat.cpp
@@ -6,13 +6,15 @@ std::string odp;
 
 Room sekretariat(GameData& gameData) {
     std::cout << "Jesteś w sekretariacie." 
-              << "Ledwo zamknąłeś drzwi, Lembas zagrodził Ci drogę." << std::endl
-              << "Zniecierpliwiony wysłuchujesz hiwait_ms(100);storii o wysyłaniu pierwszego maila w czasach paleolitu." << std::endl
+              << "Ledwo zamknąłeś drzwi, otyły mag w swetrze zagrodził Ci drogę." << std::endl
+              << "Zniecierpliwiony wysłuchujesz historii o wysyłaniu pierwszego maila w czasach paleolitu." << std::endl
               << "Jesteś odważny. Przerywasz mu." << std::endl
-              << "You triggered Lemas"
+              << "Trigerr is coming..."
               << "Dostajesz jedną szanse, jedno pytanie..." << std::endl
               << "Który język programowania jest najlepszy? " << std::endl
               << "Odpowiadasz: " << std::endl;
+              
+    std::cin >> odp;
     wait_ms(100);
     std::cout <<".";
     wait_ms(100);
@@ -21,24 +23,23 @@ Room sekretariat(GameData& gameData) {
     std::cout <<".";
     std::cout << std::endl;
 
-    std::cin >> odp;
     if(odp == "Pascal" || odp == "pascal" || odp== "PASCAL")
     {
          std::cout<<"IT'S VERY EFFECTIVE! " << std::endl
-                  <<"Odpowiedziałeś dobrze i Lembas przyznaje Ci rację." << std::endl
+                  <<"Odpowiedziałeś dobrze i mag przyznaje Ci rację." << std::endl
                   <<"Niespodziewanie rozwiewa twoje wszystkie wątpliwości." << std::endl
-                  <<"Spod swetra wyciąga jeden z wielu własnoręcznie podpisanych egzemplarzy "
+                  <<"Spod swetra wyciąga jeden z wielu  podpisanych egzemplarzy "
                   <<" <<Wstępu do Informatyki>> i wręczając radzi Ci korzystać z niego mądrze." << std::endl;
         gameData.inventory.insert("podrecznik_wdi");
 
-         std::cout<<"Odchodząc, z tylnej kieszeni wypada mu kontrowersyjne zdjęcie Kawy" << std::endl
+         std::cout<<"Odchodząc, z tylnej kieszeni wypada mu kontrowersyjne zdjęcie słynnego programisty znanego jako /Espresso/" << std::endl
                   <<"Podnosisz je? " << std::endl;
         if (yes()) {
             std::cout << "Nie jesteś w stanie patrzeć na zdjęcie, więc szybko chowasz je do kieszeni" << std::endl
                       << "Wiesz, że może Ci się później przydać." << std::endl;
                       wait_ms(100);
             std::cout << "W najgorszym przypadku wyląduje na twoim domowym ołtarzyku"<< std::endl;
-            gameData.inventory.insert("zdjecie_kawa");
+            gameData.inventory.insert("zdjecie");
         }
                 // miejsce na przejście pizzą czekających na legitymacje studentów, ale na razie nie mam czasu,
                 // plus nie do końca czaję jak zaimplementować to, że ktoś coś "wykorzystuje" z inventory i potem już tego nie ma,
@@ -46,8 +47,7 @@ Room sekretariat(GameData& gameData) {
 
         std::cout <<" Podchodzisz w końcu do upragnionej pani przy biurku." <<std::endl
                   <<" Pytasz, czy możesz przepisać się do innej grupy z Programowania 1."<< std::endl
-                  <<" Uściślasz, że chcesz zamienić grupę Kawy, na grupę Szufy" << std::endl
-                  <<" Pani patrzy na Ciebie z litością. Prawdziwą kurwa litością." << std::endl
+                  <<" Pani patrzy na Ciebie z litością. Prawdziwą kur!#a litością." << std::endl
                   <<" Dodaje, że wnioski leżą przy drzwiach i musisz skolekcjonować dwa podpisy prowadzących" << std::endl
                   <<" #If_that_wasn't_so_obvious" << std::endl
                   <<" Zrezygnowany opuszczasz sekretariat, chowając wniosek do kieszeni." <<std::endl
@@ -60,7 +60,7 @@ Room sekretariat(GameData& gameData) {
     else
     {
         std::cout<<"Odpowiedziałeś  Ź L E" << std::endl
-                 <<"Lembas każe Ci wypierdalać w podskokach  :emoticon_cry: " << std::endl;
+                 <<"Mag każe Ci opuścić jego teren w podskokach  :emoticon_cry: " << std::endl;
     }   return WINDA_PIETRO;
     
 }
