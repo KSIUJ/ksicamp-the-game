15abcbd959865243a16b1064a82984df519d0cd5
Kacper Stysiński <kaacper1112@gmail.com>
2018-11-17 01:50:04 +0100

    unused parameter fix

--- rooms/windaPietro.cpp
+++ rooms/windaPietro.cpp
@@ -2,11 +2,11 @@
 
 #include "../main.hpp"
 
-Room windaPietro (GameData& gameData)
+Room windaPietro (__attribute__((unused))GameData& gameData)
 {
 	std::cout << "Winda nie działa, trzeba iść schodami."; 
 	wait_s(2);
 	std::cout << std::endl;
-	
+
 	return WINDA_PARTER;
 }
